e21c8b19795bab6c7c7acc37b16699e2e69e5513
hhsjsth <is.gutaorui@outlook.com>
2022-11-27 23:49:24 +0800

    所有权转移后不能使用

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,7 @@
+#![allow(unused)]
 fn main() {
-    // 填空
-    let b = false;
+    let s1 = String::from("hello");
+    let s2 = s1;
 
-    let _v = match b {
-        true => 1,
-        // 发散函数也可以用于 `match` 表达式，用于替代任何类型的值
-        false => {
-            println!("Success!");
-            panic!("we have no value for `false`, but we can panic")
-        }
-    };
-
-    println!("Exercise Failed if printing out this line!");
+    println!("{}, world!", s1);
 }
