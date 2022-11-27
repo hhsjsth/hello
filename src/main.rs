7b94f98f614d3150e57fd780bf94ab34cec4fa6c
hhsjsth <is.gutaorui@outlook.com>
2022-11-27 12:16:58 +0800

    发散函数用于 match 表达式

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 fn main() {
-    // 不要修改下面两行代码!
-    let (x, y) = (1, 2);
-    let s = sum(x, y);
+    // 填空
+    let b = false;
 
-    assert_eq!(s, 3);
-}
+    let _v = match b {
+        true => 1,
+        // 发散函数也可以用于 `match` 表达式，用于替代任何类型的值
+        false => {
+            println!("Success!");
+            panic!("we have no value for `false`, but we can panic")
+        }
+    };
 
-fn sum(x: i32, y: i32) -> i32 {
-    x + y
+    println!("Exercise Failed if printing out this line!");
 }
