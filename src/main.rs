0536c5b5d7d7f8edca5f89cdbc78a38cb6275dff
hhsjsth <is.gutaorui@outlook.com>
2022-11-28 23:45:48 +0800

    部分 move

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
-// 修复错误，不要删除任何代码行
 fn main() {
-    let s = String::from("hello, world");
+    #[derive(Debug)]
+    struct Person {
+        name: String,
+        age: Box<u8>,
+    }
 
-    print_str(&s);
+    let person = Person {
+        name: String::from("Alice"),
+        age: Box::new(20),
+    };
 
-    println!("{}", s);
-}
+    // 通过这种解构式模式匹配，person.name 的所有权被转移给新的变量 `name`
+    // 但是，这里 `age` 变量确是对 person.age 的引用, 这里 ref 的使用相当于: let age = &person.age
+    let Person { name, ref age } = person;
 
-fn print_str(s: &String) {
-    println!("{}", *s)
+    println!("The person's age is {}", age);
+
+    println!("The person's name is {}", name);
+
+    // Error! 原因是 person 的一部分已经被转移了所有权，因此我们无法再使用它
+    //println!("The person struct is {:?}", person);
+
+    // 虽然 `person` 作为一个整体无法再被使用，但是 `person.age` 依然可以使用
+    println!("The person's age from person struct is {}", person.age);
 }
