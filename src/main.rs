8b4a8b891fbbf46ec3770b8d875f5ac10b61c7fd
hhsjsth <is.gutaorui@outlook.com>
2022-11-28 23:20:34 +0800

    函数传值

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
+// 不要修改 main 中的代码
 fn main() {
-    // 使用尽可能多的方法来通过编译
-    let x = String::from("hello, world");
-    let y = &x;
-    println!("{},{}", x, *y);
+    let s1 = String::from("hello, world");
+    let s2 = take_ownership(s1);
+
+    println!("{}", s2);
+}
+
+// 只能修改下面的代码!
+fn take_ownership(s: String) -> String {
+    println!("{}", s);
+    s
 }
