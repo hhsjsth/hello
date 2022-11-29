b65e9da3a4d0819c7cc6c5fb361d3d460e4ece34
hhsjsth <is.gutaorui@outlook.com>
2022-11-29 23:14:25 +0800

    大括号限制可变引用的范围

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,10 @@
+#![allow(unused)]
 fn main() {
-    #[derive(Debug)]
-    struct Person {
-        name: String,
-        age: Box<u8>,
-    }
+    let mut s = String::from("hello");
 
-    let person = Person {
-        name: String::from("Alice"),
-        age: Box::new(20),
-    };
+    {
+        let r1 = &mut s;
+    } // r1 在这里离开了作用域，所以我们完全可以创建一个新的引用
 
-    // 通过这种解构式模式匹配，person.name 的所有权被转移给新的变量 `name`
-    // 但是，这里 `age` 变量确是对 person.age 的引用, 这里 ref 的使用相当于: let age = &person.age
-    let Person { name, ref age } = person;
-
-    println!("The person's age is {}", age);
-
-    println!("The person's name is {}", name);
-
-    // Error! 原因是 person 的一部分已经被转移了所有权，因此我们无法再使用它
-    //println!("The person struct is {:?}", person);
-
-    // 虽然 `person` 作为一个整体无法再被使用，但是 `person.age` 依然可以使用
-    println!("The person's age from person struct is {}", person.age);
+    let r2 = &mut s;
 }
