d262385136b8a60db1fe95a903bdb8abb9ff906f
hhsjsth <is.gutaorui@outlook.com>
2022-11-29 23:22:32 +0800

    生命周期

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,5 @@
-#![allow(unused)]
-fn main() {
-    let mut s = String::from("hello");
+fn no_dangle() -> String {
+    let s = String::from("hello");
 
-    {
-        let r1 = &mut s;
-    } // r1 在这里离开了作用域，所以我们完全可以创建一个新的引用
-
-    let r2 = &mut s;
+    &s
 }
