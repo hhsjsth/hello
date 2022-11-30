6fad8cefabc22023a81cccbb4e26d08f44ba37b5
hhsjsth <is.gutaorui@outlook.com>
2022-11-30 22:40:32 +0800

    部分移动

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-fn no_dangle() -> String {
-    let s = String::from("hello");
+fn main() {
+    let t = (String::from("hello"), String::from("world"));
 
-    &s
+    let _s = t.0;
+
+    // 仅修改下面这行代码，且不要使用 `_s`
+    println!("{:?}", t.1);
 }
