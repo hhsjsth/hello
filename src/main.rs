b596de4d62f45ed9977d3450d596cb688bcde075
hhsjsth <is.gutaorui@outlook.com>
2022-12-05 22:16:57 +0800

    string concaternate 的 move

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 fn main() {
-    let string_append = String::from("hello ");
-    let string_rust = String::from("rust");
-    // &string_rust会自动解引用为&str
-    let result = string_append + &string_rust;
-    let mut result = result + "!";
-    result += "!!!";
-
-    println!("连接字符串 + -> {}", result);
+    let mut s1 = String::from("hello,");
+    let s2 = String::from("world!");
+    // 在下句中，s1的所有权被转移走了，因此后面不能再使用s1
+    s1 = s1 + &s2;
+    // 下面的语句如果去掉注释，就会报错
+    println!("{}", s1);
 }
