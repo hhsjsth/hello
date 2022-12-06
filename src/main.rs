5df5015647e44e42db080f220a092718ff71ae94
hhsjsth <is.gutaorui@outlook.com>
2022-12-06 22:47:31 +0800

    box into

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+// 使用至少两种方法来修复错误
 fn main() {
-    let s1 = "hello";
-    let s2 = String::from("rust");
-    let s = format!("{} {}!", s1, s2);
-    println!("{}", s);
-    println!("{}", s1);
-    println!("{}", s2);
+    let s: Box<str> = "hello, world".into();
+    greetings(&s);
+}
+
+fn greetings(s: &str) {
+    println!("{}", s)
 }
