8a04d06cf94dd469db4f9e0ab1dd186483aa8474
hhsjsth <is.gutaorui@outlook.com>
2022-12-12 00:00:01 +0800

    char 类型是 Unicode 编码

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 fn main() {
-    let string_append = String::from("hello ");
-    let string_rust = String::from("rust");
-    // &string_rust会自动解引用为&str
-    let result = string_append + &string_rust;
-    let mut result = result + "!";
-    result += "!!!";
+    let arr: [char; 3] = ['中', '国', '人'];
 
-    println!("连接字符串 + -> {}", result);
+    let slice = &arr[..2];
+
+    // 修改数字 `8` 让代码工作
+    // 小提示: 切片和数组不一样，它是引用。如果是数组的话，那下面的 `assert!` 将会通过： '中'和'国'是char类型，char类型是Unicode编码，大小固定为4字节，两个字符为8字节。
+    assert!(std::mem::size_of_val(&slice) == 16);
 }
