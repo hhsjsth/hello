a7d83897d1bd2d5e0f5c0865f9eb05a69b40599e
hhsjsth <is.gutaorui@outlook.com>
2023-01-03 22:54:18 +0800

    自定义类型的打印输出

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,49 @@
-// 填空
-fn print_array<T: std::fmt::Debug, const N: usize>(arr: [T; N]) {
-    println!("{:?}", arr);
-}
-fn main() {
-    let arr = [1, 2, 3];
-    print_array(arr);
+#![allow(dead_code)]
 
-    let arr = ["hello", "world"];
-    print_array(arr);
+use std::fmt;
+use std::fmt::Display;
+
+#[derive(Debug, PartialEq)]
+enum FileState {
+    Open,
+    Closed,
+}
+
+#[derive(Debug)]
+struct File {
+    name: String,
+    data: Vec<u8>,
+    state: FileState,
+}
+
+impl Display for FileState {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            FileState::Open => write!(f, "OPEN"),
+            FileState::Closed => write!(f, "CLOSED"),
+        }
+    }
+}
+
+impl Display for File {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "<{} ({})>", self.name, self.state)
+    }
+}
+
+impl File {
+    fn new(name: &str) -> File {
+        File {
+            name: String::from(name),
+            data: Vec::new(),
+            state: FileState::Closed,
+        }
+    }
+}
+
+fn main() {
+    let f6 = File::new("f6.txt");
+    //...
+    println!("{:?}", f6);
+    println!("{}", f6);
 }
