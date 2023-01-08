562b55bfe1ef4c13882055edaf480daefa221631
hhsjsth <is.gutaorui@outlook.com>
2023-01-08 22:47:43 +0800

    特征对象

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,39 @@
-// 填空
-fn example1() {
-    // `T: Trait` 是最常使用的方式
-    // `T: Fn(u32) -> u32` 说明 `T` 只能接收闭包类型的参数
-    struct Cacher<T: Fn(u32) -> u32> {
-        calculation: T,
-        value: Option<u32>,
-    }
-
-    impl<T: Fn(u32) -> u32> Cacher<T> {
-        fn new(calculation: T) -> Cacher<T> {
-            Cacher {
-                calculation,
-                value: None,
-            }
-        }
-
-        fn value(&mut self, arg: u32) -> u32 {
-            match self.value {
-                Some(v) => v,
-                None => {
-                    let v = (self.calculation)(arg);
-                    self.value = Some(v);
-                    v
-                }
-            }
-        }
-    }
-
-    let mut cacher = Cacher::new(|x| x + 1);
-    assert_eq!(cacher.value(10), 10);
-    assert_eq!(cacher.value(15), 15);
+trait Draw {
+    fn draw(&self) -> String;
 }
 
-fn example2() {
-    // 还可以使用 `where` 来约束 T
-    struct Cacher<T>
-    where
-        T: Fn(u32) -> u32,
-    {
-        calculation: T,
-        value: Option<u32>,
+impl Draw for u8 {
+    fn draw(&self) -> String {
+        format!("u8: {}", *self)
     }
+}
 
-    impl<T> Cacher<T>
-    where
-        T: Fn(u32) -> u32,
-    {
-        fn new(calculation: T) -> Cacher<T> {
-            Cacher {
-                calculation,
-                value: None,
-            }
-        }
-
-        fn value(&mut self, arg: u32) -> u32 {
-            match self.value {
-                Some(v) => v,
-                None => {
-                    let v = (self.calculation)(arg);
-                    self.value = Some(v);
-                    v
-                }
-            }
-        }
+impl Draw for f64 {
+    fn draw(&self) -> String {
+        format!("f64: {}", *self)
     }
+}
 
-    let mut cacher = Cacher::new(|x| x + 1);
-    assert_eq!(cacher.value(20), 20);
-    assert_eq!(cacher.value(25), 25);
+// 若 T 实现了 Draw 特征， 则调用该函数时传入的 Box<T> 可以被隐式转换成函数参数签名中的 Box<dyn Draw>
+fn draw1(x: Box<dyn Draw>) {
+    // 由于实现了 Deref 特征，Box 智能指针会自动解引用为它所包裹的值，然后调用该值对应的类型上定义的 `draw` 方法
+    x.draw();
+}
+
+fn draw2(x: &dyn Draw) {
+    x.draw();
 }
 
 fn main() {
-    example1();
-    example2();
+    let x = 1.1f64;
+    // do_something(&x);
+    let y = 8u8;
 
-    println!("Success!")
+    // x 和 y 的类型 T 都实现了 `Draw` 特征，因为 Box<T> 可以在函数调用时隐式地被转换为特征对象 Box<dyn Draw>
+    // 基于 x 的值创建一个 Box<f64> 类型的智能指针，指针指向的数据被放置在了堆上
+    draw1(Box::new(x));
+    // 基于 y 的值创建一个 Box<u8> 类型的智能指针
+    draw1(Box::new(y));
+    draw2(&x);
+    draw2(&y);
 }
