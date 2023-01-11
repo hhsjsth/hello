8ad82bd03f731d539604b37becdc5f8328894c3c
hhsjsth <is.gutaorui@outlook.com>
2023-01-11 23:51:58 +0800

    newtype

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,14 @@
-trait Bird {
-    fn quack(&self);
-}
+use std::fmt;
 
-struct Duck;
-impl Duck {
-    fn fly(&self) {
-        println!("Look, the duck is flying")
-    }
-}
-struct Swan;
-impl Swan {
-    fn fly(&self) {
-        println!("Look, the duck.. oh sorry, the swan is flying")
-    }
-}
+struct Wrapper(Vec<String>);
 
-impl Bird for Duck {
-    fn quack(&self) {
-        println!("{}", "duck duck");
-    }
-}
-
-impl Bird for Swan {
-    fn quack(&self) {
-        println!("{}", "swan swan");
+impl fmt::Display for Wrapper {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "[{}]", self.0.join(", "))
     }
 }
 
 fn main() {
-    // 填空
-    let birds: &[Box<dyn Bird>; 2] = &[Box::new(Duck), Box::new(Swan)];
-
-    for bird in birds {
-        bird.quack();
-        // 当 duck 和 swan 变成 bird 后，它们都忘了如何翱翔于天际，只记得该怎么叫唤了。。
-        // 因此，以下代码会报错
-        // bird.fly();
-    }
+    let w = Wrapper(vec![String::from("hello"), String::from("world")]);
+    println!("w = {}", w);
 }
