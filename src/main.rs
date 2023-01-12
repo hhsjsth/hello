4bd0c82de5e0105a6667188e21eb24f28796b760
hhsjsth <is.gutaorui@outlook.com>
2023-01-12 23:52:14 +0800

    newtype

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use std::fmt;
 
-struct Wrapper(Vec<String>);
+// 定义一个 newtype `Pretty`
+struct Pretty(String);
 
-impl fmt::Display for Wrapper {
+impl fmt::Display for Pretty {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "[{}]", self.0.join(", "))
+        write!(f, "\"{}\"", self.0.clone() + ", world")
     }
 }
 
 fn main() {
-    let w = Wrapper(vec![String::from("hello"), String::from("world")]);
+    let w = Pretty("hello".to_string());
     println!("w = {}", w);
 }
