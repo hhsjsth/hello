09abeebb127a77b6137209181aa9924867e7428e
hhsjsth <is.gutaorui@outlook.com>
2023-01-15 01:31:20 +0800

    内存地址转换为指针

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,12 @@
-#[derive(Debug)]
-enum IpAddr {
-    V4(String),
-    V6(String),
-}
+#![allow(unused)]
 fn main() {
-    let v = vec![
-        IpAddr::V4("127.0.0.1".to_string()),
-        IpAddr::V6("::1".to_string()),
-    ];
-
-    for ip in v {
-        show_addr(ip)
+    let mut values: [i32; 2] = [1, 2];
+    let p1: *mut i32 = values.as_mut_ptr();
+    let first_address = p1 as usize; // 将p1内存地址转换为一个整数
+    let second_address = first_address + 4; // 4 == std::mem::size_of::<i32>()，i32类型占用4个字节，因此将内存地址 + 4
+    let p2 = second_address as *mut i32; // 访问该地址指向的下一个整数p2
+    unsafe {
+        *p2 += 1;
     }
-}
-
-fn show_addr(ip: IpAddr) {
-    println!("{:?}", ip);
+    assert_eq!(values[1], 3);
 }
