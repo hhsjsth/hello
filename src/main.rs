8478486f8420033b7c820631d04d58ca47d2512a
hhsjsth <is.gutaorui@outlook.com>
2023-01-15 17:15:28 +0800

    tryinto

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
-#![allow(unused)]
 fn main() {
-    let mut values: [i32; 2] = [1, 2];
-    let p1: *mut i32 = values.as_mut_ptr();
-    let first_address = p1 as usize; // 将p1内存地址转换为一个整数
-    let second_address = first_address + 4; // 4 == std::mem::size_of::<i32>()，i32类型占用4个字节，因此将内存地址 + 4
-    let p2 = second_address as *mut i32; // 访问该地址指向的下一个整数p2
-    unsafe {
-        *p2 += 1;
-    }
-    assert_eq!(values[1], 3);
+    let b: i16 = 1500;
+
+    let b_: u8 = match b.try_into() {
+        Ok(b1) => b1,
+        Err(e) => {
+            println!("{:?}", e.to_string());
+            0
+        }
+    };
 }
