a9101a09629139389f17aa458cb7e98fc290d403
hhsjsth <is.gutaorui@outlook.com>
2023-01-16 23:17:02 +0800

    模块

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 #![allow(unused)]
 fn main() {
-    use std::fs::File;
-    use std::io;
-    use std::io::Read;
+    // 餐厅前厅，用于吃饭
+    mod front_of_house {
+        mod hosting {
+            fn add_to_waitlist() {}
 
-    fn read_username_from_file() -> Result<String, io::Error> {
-        let mut f = File::open("hello.txt")?;
-        let mut s = String::new();
-        f.read_to_string(&mut s)?;
-        Ok(s)
+            fn seat_at_table() {}
+        }
+
+        mod serving {
+            fn take_order() {}
+
+            fn serve_order() {}
+
+            fn take_payment() {}
+        }
     }
 }
