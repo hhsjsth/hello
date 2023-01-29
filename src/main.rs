6c523ea00b6dc49c73c4304a82ee864db76d91b0
hhsjsth <is.gutaorui@outlook.com>
2023-01-29 20:32:04 +0800

    lib.rs

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,6 @@ pub mod garden;
 fn main() {
     let plant = Asparagus {};
     println!("I'm growing {:?}!", plant);
-    let i: String = 0.0;
-    let i: String = 0.0;
+    let i: String = "asdfasdf".to_string();
+    let ii: String = String::new("asdfasdf");
 }
