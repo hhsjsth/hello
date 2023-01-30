1c9fb2fdb25bd8eef8109f1fd8d7b5fe3ba6daec
hhsjsth <is.gutaorui@outlook.com>
2023-01-30 09:35:34 +0800

    20230130

--- src/garden/vegetables.rs
+++ src/garden/vegetables.rs
@@ -1,2 +1,4 @@
 #[derive(Debug)]
-pub struct Asparagus {}
+pub struct Asparagus {
+
+}
--- src/main.rs
+++ src/main.rs
@@ -7,4 +7,5 @@ fn main() {
     println!("I'm growing {:?}!", plant);
     let i: String = "asdfasdf".to_string();
     let ii: String = String::new("asdfasdf");
+    let ii: String = String;
 }
