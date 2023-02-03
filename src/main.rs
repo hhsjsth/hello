3bdcaa1f59c625cda6604ce92fff898ed6f0ea02
hhsjsth <is.gutaorui@outlook.com>
2023-02-03 22:48:44 +0800

    display

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,16 @@
-/**
- * 你好
- * 我是
- * Rust
- * 语言
- */
-mod back_of_house {
-    pub struct Breakfast {
-        pub toast: String,
-        seasonal_fruit: String,
-    }
-
-    impl Breakfast {
-        pub fn summer(toast: &str) -> Breakfast {
-            Breakfast {
-                toast: String::from(toast),
-                seasonal_fruit: String::from("peaches"),
-            }
-        }
-    }
+#[derive(Debug)]
+struct Person {
+    name: String,
+    age: u8,
 }
 
-pub fn eat_at_restaurant() {
-    // Order a breakfast in the summer with Rye toast
-    let mut meal = back_of_house::Breakfast::summer("Rye");
-    // Change our mind about what bread we'd like
-    meal.toast = String::from("Wheat");
-    println!("I'd like {} toast please", meal.toast);
-
-    // The next line won't compile if we uncomment it; we're not allowed
-    // to see or modify the seasonal fruit that comes with the meal
-    // meal.seasonal_fruit = String::from("blueberries");
+fn main() {
+    let i = 3.1415926;
+    let s = String::from("hello");
+    let v = vec![1, 2, 3];
+    let p = Person {
+        name: "sunface".to_string(),
+        age: 18,
+    };
+    println!("{}, {}, {:#?}, {:#?}", i, s, v, p);
 }
